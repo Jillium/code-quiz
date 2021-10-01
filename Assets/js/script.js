23e59b24665fc7fc1fea78fa42ecf37a915ce844
Jillium <86424914+Jillium@users.noreply.github.com>
2021-09-30 18:36:13 -0600

    progress save

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,11 +1,46 @@
 var timerEl = document.getElementById("countdown");
 var quizDivEl = document.getElementById("quiz-name-div");
 var quizQuestEl = document.getElementById("hidden-question");
+// var score = 0;
 
+// questions and answers stored as object 
+var quizQuestions = 
+    {
+    title: 'Which of the following is not the way to declare a Javascript variable?',
+    options: ['Var', 'Let', 'Variable', 'Const'],
+    correctAnswer: 2,
+    }
+    // {
+    // title: 'Which of the following is not a Javascript Data Type?',
+    // options: ['Data', 'Number', 'String', 'Object'],
+    // correctAnswer: 0,    
+    // },
+    // {
+    // title: 'How do you start a comment in Javascript?',
+    // options: ['<!--', '//', 'Comment', '/*'],
+    // correctAnswer: 1,    
+    // },
+    // {
+    // title: 'Which Javascript data type can only have a value of true/false?',
+    // options: ['Number', 'bigInt', 'Boolean', 'Object'],
+    // correctAnswer: 2,    
+    // }
+// ];
 
+// for(var i=0; i < quizQuestions.length; i++) {
+//function to put quizquestions on the page 
+function askQuizQuestion () {
+    var quizQuestionsDiv = document.getElementById("question-div");
+    quizQuestionsDiv.textContent = quizQuestions.title;
+    var answersOptions = document.querySelectorAll('.answers');
+    answersOptions.forEach(function(element, index) {
+        element.textContent = quizQuestions.options[element, index];
+    });
+   
+}
+// };
 
-
-
+askQuizQuestion ();
 
 
 //When I click start the countdown timer starts counting
@@ -15,6 +50,7 @@ var btn = document.getElementById("start-button");
 btn.addEventListener("click", function() {
     quizDivEl.setAttribute("style", "display:none");
     quizQuestEl.setAttribute("style", "display: block");
+
 
 
     countdownTimer();
