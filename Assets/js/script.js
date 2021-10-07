7636f9d811b2569d4e60622be8d8456c48458ae3
Jillium <86424914+Jillium@users.noreply.github.com>
2021-10-07 11:19:23 -0600

    questions and answers working properly

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,7 +10,16 @@ var questionTextEl = document.getElementById("question-text");
 var quizScoreEl = document.getElementById("hidden-score-container");
 var submitButton = document.getElementById("submit-button");
 var controlsEl = document.getElementById("hidden-controls-container");
+var answerEl = document.getElementById("options");
 var answerButtons = document.getElementsByClassName("answers");
+var btn = document.createElement("button");
+btn.className += "answers";
+var btn2 = document.createElement("button");
+btn2.className += "answers";
+let btn3 = document.createElement("button");
+btn3.className += "answers";
+let btn4 = document.createElement("button");
+btn4.className += "answers";
 
 
 
@@ -18,22 +27,47 @@ var score = 0;
 
 
 
-// questions and answers stored as object
-var quizQuestions = ['Question One', 'Question two', 'Question three', 'Question 4'];
-index = 0;
+// questions 
+var quizQuestions = ['What is the technique to invisibly graft live stitches together?', 'In addition to the knit stitch, what is the second basic knitting stitch?', 'Where does mohair yarn come from?', 'What is gauge in knitting patterns?'];
+
+// answers for each button are stored in separate arrays 
+var buttonOneAnswers = ['Cast On', 'Knot', 'Rabbit', 'Number of stitches and rows per inch', 'Thickness of needles', 'Weight of yarn', 'Width of yarn'];
+var buttonTwoAnswers = ['Kitchener', 'Pearl', 'Cow', 'Thickness of needles'];
+var buttonThreeAnswers = ['Brioche', 'Purl', 'Dog', 'Weight of yarn'];
+var buttonFourAnswers = ['Short Rows', 'Slip', 'Goat', 'Width of yarn'];
+
+// index for the questions
+var quizQuestionindex = 0;
+
+// indexes for the buttons
+var buttonOneIndex = 0;
+var buttonTwoIndex = 0;
+var buttonThreeIndex = 0;
+var buttonFourIndex = 0;
 
 
 
 
-
-
+// on startbutton click the questions and answers appear to the page 
 function quizStart() {
     startButton.addEventListener("click", function () {
         quizDivEl.setAttribute("style", "display:none");
         quizQuestEl.setAttribute("style", "display: block");
         controlsEl.setAttribute("style", "display: block;");
         questionTextEl.innerText = quizQuestions[0];
-      
+        
+        btn.textContent = buttonOneAnswers[0];
+        answerEl.appendChild(btn);
+        
+        btn2.textContent = buttonTwoAnswers[0];
+        answerEl.appendChild(btn2);
+        
+        btn3.textContent = buttonThreeAnswers[0];
+        answerEl.appendChild(btn3);
+        
+        btn4.textContent = buttonFourAnswers[0];
+        answerEl.appendChild(btn4);
+       
 
 
 
@@ -50,17 +84,35 @@ function quizStart() {
 }
 
 nextButton.addEventListener("click", nextQuestion);
-
+// questions and answers are flipped through 
 function nextQuestion() {
     index++;
-    index %= quizQuestions.length
+    index %= quizQuestions.length;
+    // answerIndex++;
+    // answerIndex %= quizAnswers.length;
     questionTextEl.innerText = quizQuestions[index];
+    buttonOneIndex++
+    buttonOneIndex %= buttonOneAnswers.length;
+    btn.textContent = buttonOneAnswers[index];
+
+    buttonTwoIndex++
+    buttonTwoIndex %= buttonTwoAnswers.length;
+    btn2.textContent = buttonTwoAnswers[index];
+
+    buttonThreeIndex++
+    buttonThreeIndex %= buttonThreeAnswers.length;
+    btn3.textContent = buttonThreeAnswers[index];
+
+    buttonFourIndex++
+    buttonFourIndex %= buttonFourAnswers.length;
+    btn4.textContent = buttonFourAnswers[index];
+    
 }
 
 
 
 
-
+// function to end the quiz 
 
 
 
@@ -98,8 +150,7 @@ function countdownTimer() {
 
 //if score wrong or correct display result
 // if wrong take time off the timer 
-// move to next question
-//go through questions
+
 //score tally @end
 //text box for initials
 //save initials and score to local storage
